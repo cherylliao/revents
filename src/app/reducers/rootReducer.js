6a22152005bcf58660930dca20b234ac3b417444
Cheryl <liaochery@yahoo.com>
2019-10-25 14:19:38 +0800

    reduced events

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "../../../../Library/Caches/typescript/3.6/node_modules/redux";
 import testReducer from "../../features/testarea/testReducer";
+import eventReducer from "../../features/event/eventReducer";
 
 const rootReducer = combineReducers({
-    test:testReducer
+    test:testReducer,
+    events:eventReducer
 })
 
 export default rootReducer;
--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -3,64 +3,24 @@ import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
 import cuid from 'cuid';
+import {connect} from 'react-redux'
+import {deleteEvent,updateEvent,createEvent} from '../eventActions'
 
-const eventsFromDashboard = [
-  {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28',
-    category: 'drinks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-]
+const mapState = state =>({
+  events:state.events
+})
+const actions = {
+  createEvent,
+  deleteEvent,
+  updateEvent
+} 
 
 
-export default function EventDashboard() {
-    const [events,setEvents] =useState(eventsFromDashboard);
+const EventDashboard=({events,deleteEvent,updateEvent,createEvent})=> {
+    
     const [isOpen,setOpen] = useState(false);
     const [selectedEvent,setSelectedEvent] = useState(null);
-
+    
     // const handleFormOpen =() =>{
     //     !isOpen ? setOpen(true) : setOpen(false);
     // }
@@ -95,7 +55,7 @@ export default function EventDashboard() {
     const handleCreateEvent =(newEvent) => {
         newEvent.id=cuid();
         newEvent.hostPhotoURL = 'assets/user.png'
-        setEvents([...events,newEvent])
+        createEvent(newEvent)
         setOpen(false)
     }
 
@@ -105,22 +65,13 @@ export default function EventDashboard() {
 
     }
     const handleUpdateEvent = updatedEvent =>{
-        setEvents(events.map(event=>{
-            if(event.id===updatedEvent.id){
-                return{...updatedEvent}
-            }else{
-                return event
-            }
-        }
-        )
-        
-        )
+        updateEvent(updatedEvent)
         setOpen(false)
         setSelectedEvent(null)
     }
     const handleDeleteEvent = id =>{
-        setEvents(events.filter(e=>e.id!==id)
-        )}
+        deleteEvent(id)
+        }
     return (
         <div>
             <Grid>
@@ -144,3 +95,4 @@ export default function EventDashboard() {
         </div>
     )
 }
+export default connect(mapState,actions)(EventDashboard)
