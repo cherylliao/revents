b479aabca73a456c1841ec13ddebb34da32ffae5
Cheryl <liaochery@yahoo.com>
2019-10-26 19:21:47 +0800

    date

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -1,21 +1,25 @@
 import React,{useState} from "react";
 import DatePicker from "react-datepicker";
+import {Form,Label} from 'semantic-ui-react'
  
 import "react-datepicker/dist/react-datepicker.css";
 
 
 
-const DateInput = () => {
+const DateInput = ({meta:{touched,error},...rest}) => {
     const [date,setDate] = useState(new Date())
 
     const handleChange = date => {
         setDate(date);
       };
   return (
-    <DatePicker
+      <Form.Field error={touched && !!error}>
+    <DatePicker {...rest}
         selected={date}
         onChange={handleChange}
       />
+      {touched && error && <Label basic color ='red'>{error}</Label>}
+      </Form.Field>
   )
 }
 
